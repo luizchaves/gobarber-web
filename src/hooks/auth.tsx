5e19b2a0115ee42fdf7f28a81b60d7aa3020f20b
Luiz Chaves <lucachaves@gmail.com>
2020-06-26 00:58:01 -0300

    criando header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,9 +1,15 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -12,7 +18,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -48,6 +54,8 @@ const AuthProvider: React.FC = ({ children }) => {
   const signOut = useCallback(() => {
     localStorage.removeItem('@GoBarber:token');
     localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
   }, []);
 
   return (
